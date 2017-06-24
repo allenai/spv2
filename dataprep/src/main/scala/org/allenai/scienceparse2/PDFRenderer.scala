36f063fcbdc4df0b35fea76dca40c76a34358217
Nicholas Lourie <nicholasl@allenai.org>
2017-06-23 17:13:27 -0700

    Implement changes for code review

--- dataprep/src/main/scala/org/allenai/scienceparse2/PDFRenderer.scala
+++ dataprep/src/main/scala/org/allenai/scienceparse2/PDFRenderer.scala
@@ -4,87 +4,83 @@ import org.apache.pdfbox.tools.PDFToImage
 
 object PDFRenderer {
   // see org.apache.pdfbox.tools.PDFToImage for all possible arguments.
+  trait CommandConfig
+
+  case class PDFRendererConfig(
+    command: String = null,
+    commandConfig: CommandConfig = null)
+
   case class PDFToImageConfig(
     format: Option[String] = None,
     prefix: Option[String] = None,
     startPage: Option[Int] = None,
     endPage: Option[Int] = None,
     dpi: Option[Int] = None,
-    inputfile: Option[String] = None)
+    inputfile: String = null)
+      extends CommandConfig
+
   case class PreprocessPdfConfig(
-    outputFileName: Option[String] = None,
-    inputNames: Option[Seq[String]] = Some(Seq()))
-  case class PDFRendererConfig(
-    command: Option[String] = None,
-    pdfToImageConfig: Option[PDFToImageConfig] = Some(
-      PDFToImageConfig()),
-    preprocessPdfConfig: Option[PreprocessPdfConfig] = Some(
-      PreprocessPdfConfig()))
+    outputFileName: String = null,
+    inputNames: Seq[String] = Seq())
+      extends CommandConfig
+
 
   val parser = new scopt.OptionParser[PDFRendererConfig]("PDFRenderer") {
     cmd("PDFToImage")
-      .action((_, c) => c.copy(command = Some("PDFToImage")))
+      .action((_, c) => c.copy(command = "PDFToImage", commandConfig = PDFToImageConfig()))
       .text("Render and save to disk PDF pages as image files")
       .children(
-        opt[String]("format")
-          .abbr("f")
+        opt[String]('f', "format")
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(format = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(format = Some(x)))
           }),
-        opt[String]("prefix")
-          .abbr("p")
+        opt[String]('p', "prefix")
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(prefix = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(prefix = Some(x)))
           }),
-        opt[Int]("startPage")
-          .abbr("s")
+        opt[Int]('s', "startPage")
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(startPage = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(startPage = Some(x)))
           }),
-        opt[Int]("endPage")
-          .abbr("e")
+        opt[Int]('e', "endPage")
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(endPage = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(endPage = Some(x)))
           }),
-        opt[Int]("dpi")
-          .abbr("d")
+        opt[Int]('d', "dpi")
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(dpi = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(dpi = Some(x)))
           }),
         arg[String]("inputfile")
           .required()
           .action((x, c) => {
             c.copy(
-              pdfToImageConfig = c.pdfToImageConfig.map(_.copy(inputfile = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PDFToImageConfig].copy(inputfile = x))
           }))
     cmd("PreprocessPdf")
-      .action((_, c) => c.copy(command = Some("PreprocessPdf")))
+      .action((_, c) => c.copy(command = "PreprocessPdf", commandConfig = PreprocessPdfConfig()))
       .text("Extract text and other information from the PDF")
       .children(
         arg[String]("outputFileName")
           .required()
           .action((x, c) => {
             c.copy(
-              preprocessPdfConfig = c.preprocessPdfConfig.map(_.copy(outputFileName = Some(x))))
+              commandConfig = c.commandConfig.asInstanceOf[PreprocessPdfConfig].copy(outputFileName = x))
           }),
         arg[Seq[String]]("inputNames")
           .required()
           .unbounded()
           .action((x, c) => {
             c.copy(
-              preprocessPdfConfig = c.preprocessPdfConfig.map(preprocessPdfConfig => {
-                preprocessPdfConfig.copy(
-                  inputNames = preprocessPdfConfig.inputNames.map(names => names ++ x))
-              }))
+              commandConfig = c.commandConfig.asInstanceOf[PreprocessPdfConfig].copy(
+                inputNames = c.commandConfig.asInstanceOf[PreprocessPdfConfig].inputNames ++ x))
           }))
     checkConfig { c => c match {
-      case PDFRendererConfig(None, _, _) => failure(
-        "Please specify a command")
+      case PDFRendererConfig(null, _) => failure("Please specify a command")
       case _ => success
     }}
   }
@@ -92,15 +88,15 @@ object PDFRenderer {
   def main(args: Array[String]): Unit = {
     parser.parse(args, PDFRendererConfig()) match {
       case Some(config) => {
-        if (config.command.get == "PDFToImage") {
+        if (config.command == "PDFToImage") {
           // only use scopt for option validation, but
           // allow PDFToImage to conduct it's own option
           // parsing
           PDFToImage.main(args.drop(1))
-        } else if (config.command.get == "PreprocessPdf") {
+        } else if (config.command == "PreprocessPdf") {
           PreprocessPdf.extractText(
-            outputFileName = config.preprocessPdfConfig.get.outputFileName.get,
-            inputNames = config.preprocessPdfConfig.get.inputNames.get)
+            outputFileName = config.commandConfig.asInstanceOf[PreprocessPdfConfig].outputFileName,
+            inputNames = config.commandConfig.asInstanceOf[PreprocessPdfConfig].inputNames)
         }
       }
       case None => System.exit(1)
