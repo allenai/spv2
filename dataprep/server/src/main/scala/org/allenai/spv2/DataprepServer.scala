f5b225a10e7ac4ad4d4586f138f0ee6834b21be5
Dirk Groeneveld <dirkg@allenai.org>
2017-08-29 18:20:52 -0700

    Handle zip files and individual PDFs

--- dataprep/server/src/main/scala/org/allenai/spv2/DataprepServer.scala
+++ dataprep/server/src/main/scala/org/allenai/spv2/DataprepServer.scala
@@ -2,12 +2,16 @@ package org.allenai.spv2
 
 import java.nio.file.{ Files, Path, StandardCopyOption }
 import java.security.{ DigestInputStream, MessageDigest }
+import java.util.zip.GZIPInputStream
 import javax.servlet.http.{ HttpServletRequest, HttpServletResponse }
 
 import com.trueaccord.scalapb.json.JsonFormat
 import org.allenai.common.{ Logging, Resource }
 import org.allenai.common.ParIterator._
+import org.apache.commons.compress.archivers.ArchiveInputStream
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import org.apache.commons.io.FileUtils
 import org.eclipse.jetty.server.{ Request, Server }
 import org.eclipse.jetty.server.handler.AbstractHandler
@@ -61,13 +65,34 @@ class DataprepServer extends AbstractHandler with Logging {
   }
 
   private def handleTar(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    val tempDir = Files.createTempDirectory(this.getClass.getName)
+    handleArchive(request, response, "tar", new TarArchiveInputStream(request.getInputStream))
+  }
+
+  private def handleTargz(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    handleArchive(
+      request,
+      response,
+      "tar.gz",
+      new TarArchiveInputStream(new GzipCompressorInputStream(request.getInputStream)))
+  }
+
+  private def handleZip(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    handleArchive(request, response, "zip", new ZipArchiveInputStream(request.getInputStream))
+  }
+
+  private def handleArchive(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    suffix: String,
+    getArchiveInputStream: => ArchiveInputStream
+  ): Unit = {
+    val tempDir = Files.createTempDirectory(this.getClass.getSimpleName)
     try {
       val tarSha1 = MessageDigest.getInstance("SHA-1")
       tarSha1.reset()
       // tarSha1 will be the sha of the shas of the pdfs in the tar file
 
-      val files = Resource.using(new TarArchiveInputStream(request.getInputStream)) { tarIs =>
+      val files = Resource.using(getArchiveInputStream) { tarIs =>
         val pdfSha1 = MessageDigest.getInstance("SHA-1")
         pdfSha1.reset()
 
@@ -80,7 +105,6 @@ class DataprepServer extends AbstractHandler with Logging {
             val pdfSha1Stream = new DigestInputStream(tarIs, pdfSha1)
             val tempFile = tempDir.resolve("in-progress.pdf")
             Files.copy(pdfSha1Stream, tempFile)
-            pdfSha1Stream.close()
             val pdfSha1Bytes = pdfSha1.digest()
             tarSha1.update(pdfSha1Bytes)
             val tempFileSha = Utilities.toHex(pdfSha1Bytes)
@@ -92,7 +116,7 @@ class DataprepServer extends AbstractHandler with Logging {
       }
 
       val outputSha = Utilities.toHex(tarSha1.digest())
-      response.setHeader("Location", s"${request.getRequestURI}/$outputSha.tar")
+      response.setHeader("Location", s"${request.getRequestURI}/$outputSha.$suffix")
       response.setStatus(200) // Should be 201, but we didn't really create anything.
 
       writeResponse(files, response)
@@ -101,16 +125,24 @@ class DataprepServer extends AbstractHandler with Logging {
     }
   }
 
-  private def handleTargz(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    ???
-  }
-
-  private def handleZip(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    ???
-  }
-
   private def handlePdf(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    ???
+    val pdfSha1 = MessageDigest.getInstance("SHA-1")
+    pdfSha1.reset()
+    val pdfSha1Stream = new DigestInputStream(request.getInputStream, pdfSha1)
+
+    val tempDir = Files.createTempDirectory(this.getClass.getSimpleName)
+    try {
+      val tempFile = tempDir.resolve("in-progress.pdf")
+      Files.copy(pdfSha1Stream, tempFile)
+      val pdfSha1Bytes = pdfSha1.digest()
+      val tempFileSha = Utilities.toHex(pdfSha1Bytes)
+      val renamedFile = tempDir.resolve(tempFileSha + ".pdf")
+      Files.move(tempFile, renamedFile)
+      response.setHeader("Location", s"${request.getRequestURI}/$tempFileSha.pdf")
+      writeResponse(Seq(renamedFile), response)
+    } finally {
+      FileUtils.deleteDirectory(tempDir.toFile)
+    }
   }
 
   private def handleUrls(request: HttpServletRequest, response: HttpServletResponse): Unit = {
