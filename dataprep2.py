74af120e5a776ee704efdc3c428b504e1b44d9a6
Dirk Groeneveld <dirkg@allenai.org>
2017-09-06 13:16:11 -0700

    Bump the featurized version number for the masking fix

--- dataprep2.py
+++ dataprep2.py
@@ -861,7 +861,7 @@ def labeled_tokens_file(bucket_path: str):
 # Featurized Tokens 👣
 #
 
-FEATURIZED_TOKENS_VERSION = "12corr"
+FEATURIZED_TOKENS_VERSION = "13mask"
 
 def make_featurized_tokens_file(
     output_file_name: str,
