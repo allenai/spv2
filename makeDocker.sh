09c34fc309f700651bebde4cf469fa3780ecce35
Dirk Groeneveld <dirkg@allenai.org>
2017-09-18 16:28:27 -0700

    Docker files for deployment

---
+++ makeDocker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VERSION=1.0
+ENVIRONMENT=$1
+
+docker build -t spv2/preprocess:$VERSION.$ENVIRONMENT -f Dockerfile.preprocess --build-arg ENVIRONMENT=$ENVIRONMENT .
+docker build -t spv2/process:$VERSION.$ENVIRONMENT -f Dockerfile.process --build-arg ENVIRONMENT=$ENVIRONMENT .
