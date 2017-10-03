715e328b01648742926d0daf8e4451149e534f98
Dirk Groeneveld <dirkg@allenai.org>
2017-10-03 16:05:33 -0700

    Makes CPU support official

--- makeDocker.sh
+++ makeDocker.sh
@@ -8,9 +8,12 @@ ENVIRONMENT=$1
 
 docker build -t spv2/preprocess:$VERSION.$ENVIRONMENT -f Dockerfile.preprocess --build-arg ENVIRONMENT=$ENVIRONMENT .
 docker build -t spv2/process:$VERSION.$ENVIRONMENT -f Dockerfile.process --build-arg ENVIRONMENT=$ENVIRONMENT .
+docker build -t spv2/process-cpu:$VERSION.$ENVIRONMENT -f Dockerfile.process-cpu --build-arg ENVIRONMENT=$ENVIRONMENT .
 
 docker tag spv2/preprocess:$VERSION.$ENVIRONMENT 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/preprocess:$VERSION.$ENVIRONMENT
 docker tag spv2/process:$VERSION.$ENVIRONMENT 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/process:$VERSION.$ENVIRONMENT
+docker tag spv2/process-cpu:$VERSION.$ENVIRONMENT 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/process-cpu:$VERSION.$ENVIRONMENT
 
 docker push 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/preprocess:$VERSION.$ENVIRONMENT
 docker push 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/process:$VERSION.$ENVIRONMENT
+docker push 896129387501.dkr.ecr.us-west-2.amazonaws.com/spv2/process-cpu:$VERSION.$ENVIRONMENT
