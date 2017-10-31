9380f1849272951bd046a53c229a9720efa7d238
Dirk Groeneveld <dirkg@allenai.org>
2017-10-31 15:05:31 -0700

    Detect the version number in the dataprep dockerfile

--- dataprep/makeDocker.sh
+++ dataprep/makeDocker.sh
@@ -7,4 +7,4 @@ VERSION=$(cat version.sbt | sed -Ee "s/version in [A-Za-z]+ := \"([0-9.]+(-SNAPS
 
 find . -type d -name target | xargs rm -r
 sbt server/assembly
-docker build -t spv2/dataprep-server:$VERSION .
+docker build --build-arg version=$VERSION -t spv2/dataprep-server:$VERSION .
