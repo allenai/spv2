b3a63fc0f3c7c66074db9ac5d35ef649665ef0bb
Dirk Groeneveld <dirkg@allenai.org>
2017-11-21 15:45:09 -0800

    Fix syntax of info message

--- with_labels.py
+++ with_labels.py
@@ -1055,7 +1055,7 @@ def train(
     if training_batches > 0:
         trained_batches = 0
         while trained_batches < training_batches:
-            logging.info("Starting new epoch batches = {}".format(trained_batches))
+            logging.info("Starting new epoch. Batches trained so far: {}".format(trained_batches))
             train_docs = dataprep2.documents(
                 pmc_dir,
                 model_settings,
