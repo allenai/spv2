1f834474d46ebcba2b2c4ab6c6c58c6aaee6a116
Zheng Yuan <zhengyuan.beihang@gmail.com>
2018-01-17 10:17:42 -0600

    bucket range change

--- dataprep2.py
+++ dataprep2.py
@@ -1593,13 +1593,15 @@ def documents(
     bucket_count: typing.Optional[int] = None
 ):
     if document_set is DocumentSet.TEST:
-        buckets = range(0xf0, 0x100)
         # buckets = range(0x0b, 0x0c)
+        buckets = range(0xf0, 0x100)
     elif document_set is DocumentSet.VALIDATE:
         # buckets = range(0x0a, 0x0b)
         buckets = range(0xe0, 0xf0)
     else:
-        buckets = range(0x00, 0x0a)
+        # buckets = range(0x00, 0x0a)
+        buckets = range(0x00, 0xe0)
+
     buckets = ["%02x" % x for x in buckets]
     if bucket_count is not None:
         buckets = buckets[:bucket_count]
