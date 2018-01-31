49193744a276bf1a30c1d76475058b38043ab010
Dirk Groeneveld <dirkg@allenai.org>
2018-01-31 10:16:15 -0800

    Train with a 48k batch size

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class ModelSettings(ModelSettingsBase):
 default_model_settings = ModelSettings(
     max_page_number=50,
     font_hash_size=1024 * 4,
-    tokens_per_batch=32 * 1024,
+    tokens_per_batch=48 * 1024,
     minimum_token_frequency=10,
     layer_1_dropout=0.0,
     layer_2_dropout=0.0,
