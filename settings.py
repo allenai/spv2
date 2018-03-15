ae7466df428f8929c8aa5062c533c398d327a1ce
Dirk Groeneveld <dirkg@allenai.org>
2018-03-15 14:41:15 -0700

    44k batch size

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ class ModelSettings(ModelSettingsBase):
 default_model_settings = ModelSettings(
     max_page_number=50,
     font_hash_size=1024 * 4,
-    tokens_per_batch=32 * 1024,
+    tokens_per_batch=44 * 1024,
     embedded_tokens_fraction=0.995,
     glove_vectors="model/glove.6B.100d.txt.gz"
 )
