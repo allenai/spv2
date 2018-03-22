d3e9c8f0ee3d7f362beeb3b79001c6c00ab543c2
Dirk Groeneveld <dirkg@allenai.org>
2018-03-22 14:04:24 -0700

    Use the new tokens

--- dataprep2.py
+++ dataprep2.py
@@ -394,7 +394,7 @@ class CombinedEmbeddings(object):
 # Unlabeled Tokens 🗄
 #
 
-UNLABELED_TOKENS_VERSION = "tokens5master"
+UNLABELED_TOKENS_VERSION = "tok6"
 
 h5_unicode_type = h5py.special_dtype(vlen=np.unicode)
 
@@ -553,7 +553,7 @@ def unlabeled_tokens_file(bucket_path: str):
 
     temp_unlabeled_tokens_path = unlabeled_tokens_path + ".%d.temp" % os.getpid()
     make_unlabeled_tokens_file(
-        os.path.join(bucket_path, "tokens5.json.bz2"),
+        os.path.join(bucket_path, "tokens6.json.bz2"),
         temp_unlabeled_tokens_path)
     os.rename(temp_unlabeled_tokens_path, unlabeled_tokens_path)
     return h5py.File(unlabeled_tokens_path, "r")
@@ -563,7 +563,7 @@ def unlabeled_tokens_file(bucket_path: str):
 # Labeling 🏷
 #
 
-LABELED_TOKENS_VERSION = "22bibs"
+LABELED_TOKENS_VERSION = "tok6"
 
 _split_words_re = re.compile(r'(\W|\d+)')
 _not_spaces_re = re.compile(r'\S+')
@@ -1199,7 +1199,7 @@ def labeled_tokens_file(bucket_path: str):
 # Featurized Tokens 👣
 #
 
-FEATURIZED_TOKENS_VERSION = "22bibs"
+FEATURIZED_TOKENS_VERSION = "tok6"
 
 def make_featurized_tokens_file(
     output_file_name: str,
