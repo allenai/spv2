94392b5521bd240c6b5c458dda8e9ff296f75122
Dirk Groeneveld <dirkg@allenai.org>
2018-04-20 18:14:57 -0700

    4 parsing threads at the same time

--- dataprep/server/src/main/scala/org/allenai/spv2/DataprepServer.scala
+++ dataprep/server/src/main/scala/org/allenai/spv2/DataprepServer.scala
@@ -36,7 +36,7 @@ object DataprepServer extends Logging {
 }
 
 class DataprepServer extends AbstractHandler with Logging {
-  private val parallelParsingPermits = new Semaphore(2) // Allow two parsing threads at the same time
+  private val parallelParsingPermits = new Semaphore(4) // Allow two parsing threads at the same time
 
   private case class Route(expectedMethod: String, f: (HttpServletRequest, HttpServletResponse) => Unit)
 
